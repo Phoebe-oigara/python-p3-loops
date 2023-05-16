ff88424e87c573f72e9e1c3371cedde58860d20a
Phoebe Oigara <phoebeoigara@gmail.com>
2023-05-16 18:07:39 +0300

    looping in python

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_integers  = []
+
+    for int in int_list:
+        squared_integers.append(int ** 2)
+    return squared_integers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for n in range(1, 101):
+        if n % 3 == 0 and n % 5 == 0:
+            print("FizzBuzz")
+        elif n% 3 == 0:
+            print ("Fizz")
+        elif n % 5 == 0:
+            print ("Buzz")
+        else:
+            print(n)
